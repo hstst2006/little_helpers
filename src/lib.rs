e9eab4282eb675d2cd932441244d0ed0540e9ba4
Håvard Skordal Thorsen <hstst2006@gmail.com>
2022-12-21 00:38:13 +0100

    Organized functions into modules, added index to coordinate conversion functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,70 @@
-//! Little_helpers is a library of helper functions intended to be used for reading Advent of Code input data.
-use std::fs::{File, read_to_string};
-use std::io::{Write, Result};
+//! Little_helpers is a library of helper functions intended to be used for working with Advent of Code data.
+
 
 mod test;
+pub mod listify {
 
-/// Copies the contents of one file into a new file, with one item per line.
-/// 
-/// The items from the original file will be split at the delimiter token, and empty lines will be ignored.
-/// Returns the File handle of the new file, or an error if it fails to create the output file.
-pub fn listify_into_file(file_in: String, file_out: &str, delimiter: &str) -> Result<File> {
+    use std::fs::{File, read_to_string};
+    use std::io::{Write, Result};
 
-    let file_contents = read_to_string(file_in)?;
-    let mut output_file = File::create(&file_out)?;
-    for line in file_contents.split(delimiter).filter(|s| !s.is_empty()) {
-        writeln!(output_file, "{}", line)?;
+    /// Copies the contents of one file into a new file, with one item per line.
+    /// 
+    /// The items from the original file will be split at the delimiter token, and empty lines will be ignored.
+    /// Returns the File handle of the new file, or an error if it fails to create the output file.
+    pub fn listify_into_file(file_in: String, file_out: &str, delimiter: &str) -> Result<File> {
+
+        let file_contents = read_to_string(file_in)?;
+        let mut output_file = File::create(&file_out)?;
+        for line in file_contents.split(delimiter).filter(|s| !s.is_empty()) {
+            writeln!(output_file, "{}", line)?;
+        }
+        Ok(output_file)
     }
-    Ok(output_file)
+
+    /// Returns a Result<Vec<T>> from a file using a delimiter token.
+    /// 
+    /// Output vector only contains value types implementing the FromStr trait.
+    /// If parsing to something that is not a string type, invalid values will be omitted
+    pub fn listify_into_vec<T: std::str::FromStr>(file_in: &str, delimiter: &str) -> Result<Vec<T>>
+    {
+        let input_file = read_to_string(file_in)?;
+
+        let file_contents: Vec<T> = input_file.split(delimiter).filter_map(|val| val.parse().ok()).collect();
+        Ok(file_contents)
+    }
+
+    /// Returns a Result<Vec<Option<T>>> from a file using a lines as delimiter token
+    /// 
+    /// Output vector only contains value types implementing the FromStr trait.
+    /// If parsing to something that is not a string type, invalid lines will be None
+    pub fn listify_lines<T: std::str::FromStr>(file_in: &str) -> Result<Vec<Option<T>>> {
+        let input_file = read_to_string(file_in)?;
+
+        let file_contents: Vec<Option<T>> = input_file.lines().map(|val| val.parse().ok()).collect();
+        Ok(file_contents)
+
+    }
 }
 
-/// Returns a Result<Vec<T>> from a file using a delimiter token.
-/// 
-/// Output vector only contains value types implementing the FromStr trait.
-/// If parsing to something that is not a string type, invalid values will be omitted
-pub fn listify_into_vec<T: std::str::FromStr>(file_in: &str, delimiter: &str) -> Result<Vec<T>>
-{
-    let input_file = read_to_string(file_in)?;
+pub mod coords {
+    
+    /// Maps 2d coordinates to a 1d index based on the width of the coordinate system
+    /// Returns usize
+    pub fn coords_to_index(x: usize, y: usize, width: usize) -> usize
+    {
+        x + (y * width)
+    }
 
-    let file_contents: Vec<T> = input_file.split(delimiter).filter_map(|val| val.parse().ok()).collect();
-    Ok(file_contents)
-}
+    /// Calculates a 1d index into a 2d X-coordinate
+    pub fn index_to_x(index: usize, width: usize) -> usize
+    {
+        index % width
+    }
 
-/// Returns a Result<Vec<Option<T>>> from a file using a lines as delimiter token
-/// 
-/// Output vector only contains value types implementing the FromStr trait.
-/// If parsing to something that is not a string type, invalid lines will be None
-pub fn listify_lines<T: std::str::FromStr>(file_in: &str) -> Result<Vec<Option<T>>> {
-    let input_file = read_to_string(file_in)?;
+    /// Calculates a 1d index into a 2d Y-coordinate
+    pub fn index_to_y(index: usize, width: usize) -> usize
+    {
+        index / width
+    }
 
-    let file_contents: Vec<Option<T>> = input_file.lines().map(|val| val.parse().ok()).collect();
-    Ok(file_contents)
-
-}
-
-/// Maps 2d coordinates to a 1d index based on the width of the coordinate system
-pub fn coords_to_index(x: usize, y: usize, width: usize) -> usize
-{
-    x + (y * width)
 }
